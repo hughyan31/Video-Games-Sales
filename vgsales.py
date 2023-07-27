7336e45949f33c24d0941c573934ee64def40187
hughyan31 <hughyan31@gmail.com>
2023-07-27 19:10:36 +0100

    Update vgsales.py
    EDA V2

--- vgsales.py
+++ vgsales.py
@@ -6,8 +6,11 @@ Created on Fri Jul 14 18:49:10 2023
 """
 import numpy as np
 import pandas as pd
+import pandas_profiling as pp
 import matplotlib.pyplot as plt
 import seaborn as sns
+from tabulate import tabulate
+
 
 np.random.seed(24)
 
@@ -25,6 +28,11 @@ df.shape
 df.info()
 df.describe()
 
+#Generate a html report on the dataset
+'''
+report = pp.ProfileReport(df)
+report.to_file('VGS_report.html')
+'''
 # Check for missing data
 print(df.isnull().sum())
 # Check for unique value
@@ -70,24 +78,38 @@ def colors_from_values(values, palette_name):
     return np.array(palette).take(indices, axis=0)
 
 # bar plot
-def barplot(data,x, y, ax=None):
+
+def barplot(data, x, y, ax=None):
     data_value = data.groupby(by=[x])[y].sum()
     data_value = data_value.reset_index()
-    if x != 'Year':
-        data_value = data_value.sort_values(by=[y], ascending=False)
+    data_value = data_value.sort_values(by=[y], ascending=False);
+    
+    if x == 'Publisher' or x == 'Name':
+        data_value = data_value[:10]
+        rotation = 45
+        ha = 'right'
+        fontsize = 7
+    else:
+        rotation = 90
+        ha = 'center'
+        fontsize = None;
         
     if ax is None:
         fig, ax = plt.subplots(figsize=(10, 6))
+    
     sns.barplot(x=x, y=y, data=data_value, palette=colors_from_values(data_value[y], "YlOrRd"), ax=ax)
     ax.set_xlabel(f'{x}')
     ax.set_ylabel(f'{y}')
     ax.set_title(f'{y} by {x}') 
-    
+
     ax.set_xticks(range(len(data_value[x])))
-    ax.set_xticklabels(data_value[x].astype(int) if x == 'Year' else data_value[x], rotation=90)
+    ax.set_xticklabels(data_value[x].astype(int) if x == 'Year' else data_value[x], rotation=rotation, ha=ha, fontsize=fontsize)
     return ax
-    
-def stackedBarplot(data):
+
+
+
+
+def plot_stackedBarplot(data):
     pivot_table = data.pivot_table(index='Year', columns='Genre', values='Rank', aggfunc='count', fill_value=0)
     plt.figure(figsize=(10, 6))
     pivot_table.plot(kind='bar', stacked=True, cmap='tab20')
@@ -98,7 +120,7 @@ def stackedBarplot(data):
     plt.legend(title='Genre', loc='upper left', bbox_to_anchor=(1, 1))
     plt.show()
 
-def heatmap(data):
+def plot_heatmap(data):
     pivot_table = data.pivot_table(index='Year', columns='Genre', aggfunc='size', fill_value=0)
     palette = sns.light_palette("green", as_cmap=True)
     plt.figure(figsize=(12, 8))
@@ -107,9 +129,9 @@ def heatmap(data):
     plt.title('Number of Game Releases by Year and Genre (Heatmap)')
     plt.show()
 
-def salesPlots(data,string):
+def plot_salesPlots(data,string):
     fig, axs = plt.subplots(3, 2, figsize=(15, 15))
-    plt.subplots_adjust(wspace=0.3, hspace=0.5)  # Adjust the spacing between subplots
+    plt.subplots_adjust(wspace=0.3, hspace=0.6)  # Adjust the spacing between subplots
     # Plot Global_Sales
     barplot(data,string,'Global_Sales', ax=axs[0, 0])
     # Plot NA_Sales
@@ -122,18 +144,86 @@ def salesPlots(data,string):
     barplot(data,string,'Other_Sales', ax=axs[2, 0])
     axs[2, 1].axis('off')
     plt.show()
-    
-# Create a subplot for the countplots
-fig, axs = plt.subplots(1, 2, figsize=(15, 6))
-plt.subplots_adjust(wspace=0.3)  # Adjust the spacing between subplots
-# Plot the countplot for Genre
-countplot(df, 'Genre', 'y', ax=axs[0])
-# Plot the countplot for Platform (showing only the top 10)
-countplot(df, 'Platform', 'y', top_n=10, ax=axs[1])
-plt.show()
 
-stackedBarplot(df)
-heatmap(df)
-salesPlots(df,'Platform')
-salesPlots(df,'Year')
-salesPlots(df,'Genre')
+def plot_countplots(data) :
+# Create a subplot for the countplots
+    fig, axs = plt.subplots(1, 2, figsize=(15, 6))
+    plt.subplots_adjust(wspace=0.3)  # Adjust the spacing between subplots
+    # Plot the countplot for Genre
+    countplot(data, 'Genre', 'y', ax=axs[0])
+    # Plot the countplot for Platform (showing only the top 10)
+    countplot(data, 'Platform', 'y', top_n=10, ax=axs[1])
+    plt.show()
+
+
+def plot_line(data):
+    # Melt the DataFrame to convert sales columns into a single column
+    sales_data = data.groupby('Year').agg({
+        'NA_Sales': 'sum',
+        'EU_Sales': 'sum',
+        'JP_Sales': 'sum',
+        'Other_Sales': 'sum'
+    }).reset_index()
+    
+    melted_df = sales_data.melt(id_vars='Year', var_name='Region', value_name='Sales')
+    
+    # Create a figure and axes
+    plt.figure(figsize=(10, 6))
+    ax = plt.gca()
+    
+    # Create a multiline graph using sns.lineplot
+    sns.lineplot(x='Year', y='Sales', hue='Region', data=melted_df, ax=ax)
+    
+    # Set axis labels and title
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Sales')
+    ax.set_title('Sales by Region and Year')
+    
+    # Show the legend
+    ax.legend(title='Region', loc='upper left', bbox_to_anchor=(1, 1))
+    
+    # Show the plot
+    plt.show()
+    
+def plot_pie(data):
+    # Calculate the total sales for each region across all years
+    total_sales = data[['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']].sum()
+    
+    # Create a list of labels for the pie chart
+    labels = total_sales.index
+    
+    # Create a list of sizes (sales values) for the pie chart
+    sizes = total_sales.values
+    
+    # Create a pie chart
+    plt.figure(figsize=(8, 8))
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140, colors=['skyblue', 'lightgreen', 'lightcoral', 'lightyellow'])
+    plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    
+    # Set the title of the pie chart
+    plt.title('Proportion of Sales by Region (NA, EU, JP, Other)')
+    
+    # Show the pie chart
+    plt.show()
+    
+def best_game(data):
+    max_sales_indices = data.groupby('Year')['Global_Sales'].idxmax()
+    # Use the indices to get the rows with the highest global sales in each year
+    games_with_max_sales = data.loc[max_sales_indices]
+    # Select the columns to display in the table
+    table_columns = ['Year', 'Name', 'Global_Sales']
+    # Convert the DataFrame to a table
+    table = tabulate(games_with_max_sales[table_columns], headers='keys', tablefmt='pretty')
+    # Display the table
+    return print(table)
+
+plot_stackedBarplot(df)
+plot_heatmap(df)
+plot_salesPlots(df,'Platform')
+plot_salesPlots(df,'Genre')
+plot_salesPlots(df,'Publisher')
+plot_salesPlots(df,'Name')
+plot_pie(df)
+plot_line(df)
+best_game(df)
+
